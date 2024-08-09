f0ed7af5ce5f446cb5b0cb091322bd34562f3437
Alisher1221-ru <kr4054658@gmail.com>
2024-08-09 13:28:28 +0500

    home pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/styles/index.css";
+import HomePage from "./components/Main";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hello world</h1>
+    <HomePage />
   </StrictMode>
 );
