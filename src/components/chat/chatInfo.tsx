4adff1546037838ffaad4eaf5bdfe3aa4bae7fa8
Alisher1221-ru <kr4054658@gmail.com>
2024-09-13 13:47:28 +0500

    fix bug chat list

--- src/components/chat/chatInfo.tsx
+++ src/components/chat/chatInfo.tsx
@@ -1,7 +1,7 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Button } from "../ui/button";
 import { ArrowBigLeft, Share2 } from "lucide-react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { formatContent, shareLink } from "@/lib/utils";
 import { useChatInfo } from "@/hooks/useChat";
 
@@ -12,8 +12,13 @@ interface Props {
 const ChatInfo: FC<Props> = () => {
   const navigate = useNavigate();
   const { id } = useParams();
+  const { pathname } = useLocation();
 
-  const { data: chat } = useChatInfo(id);
+  const { data: chat, refetch } = useChatInfo(id);
+
+  useEffect(() => {
+    refetch();
+  }, [pathname]);
 
   const leave = () => {
     navigate("/chat");
